e7b20d85b3ee24fb7f570db6f5b1136f7f84894f
范洪月 <fanhongyue@zhongan.io>
2018-06-26 09:48:34 +0000

    Update templates.go

--- config/templates.go
+++ config/templates.go
@@ -32,18 +32,18 @@ const MYCONFIGTPL = `
 {
 	"base_fee":0,
 	"base_reserve":0,
-	"max_txset_size":5000,
+	"max_txset_size":1000,
 
-	"block_size":5000,
+	"block_size":2000,
 	"block_part_size":65536,
 	"disable_data_hash":false,
 	"timeout_propose":3000,
 	"timeout_propose_delta":500,
-	"timeout_prevote":1500,
+	"timeout_prevote":1000,
 	"timeout_prevote_delta":500,
-	"timeout_precommit":2000,
+	"timeout_precommit":1000,
 	"timeout_precommit_delta":500,
-	"timeout_commit":2000,
+	"timeout_commit":1000,
 	"skip_timeout_commit":false
 }
 `
