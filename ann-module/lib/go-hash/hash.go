ca14f636b3669e1f87e5cb8c43aa93d1bc10f5a6
范洪月 <fanhongyue@zhongan.io>
2019-03-28 15:00:54 +0800

    add go hash

---
+++ ann-module/lib/go-hash/hash.go
@@ -0,0 +1,52 @@
+// Copyright 2017 ZhongAn Information Technology Services Co.,Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hash
+
+import (
+	"crypto/sha256"
+
+	"golang.org/x/crypto/ripemd160"
+)
+
+type HashType string
+
+const (
+	HashTypeSha256    HashType = "sha256"
+	HashTypeRipemd160          = "ripemd160"
+)
+
+var DoHash = ripemd160Func
+
+func ConfigHasher(typ HashType) {
+	switch typ {
+	case HashTypeSha256:
+		DoHash = sha256Func
+	default:
+		DoHash = ripemd160Func
+	}
+}
+
+func sha256Func(bytes []byte) []byte {
+	hasher := sha256.New()
+	hasher.Write(bytes)
+	return hasher.Sum(nil)
+
+}
+
+func ripemd160Func(bytes []byte) []byte {
+	hasher := ripemd160.New()
+	hasher.Write(bytes)
+	return hasher.Sum(nil)
+}
