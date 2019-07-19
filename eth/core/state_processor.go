be779ab46e98a4c931cc8b116724c9ffcb197cdc
zhangjian002 <zhangjian002@zhongan.io>
2019-07-19 15:46:10 +0800

    add result value in receipt

--- eth/core/state_processor.go
+++ eth/core/state_processor.go
@@ -68,7 +68,7 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 	}
 
 	// Apply the transaction to the current state (included in the env)
-	_, gas, failed, err := ApplyMessage(vmenv, msg, gp)
+	res, gas, failed, err := ApplyMessage(vmenv, msg, gp)
 	if err != nil {
 		return nil, 0, err
 	}
@@ -88,7 +88,7 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 	// Edit by zhongan
 	txBytes, _ := rlp.EncodeToBytes(tx)
 	receipt.TxHash = common.BytesToHash(gtypes.Tx(txBytes).Hash())
-
+	receipt.Result = res
 	receipt.GasUsed = gas
 	// if the transaction created a contract, store the creation address in the receipt.
 	if msg.To() == nil {
